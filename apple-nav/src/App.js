53b3c01e1f9ddb99c937f8395158637208e8448d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-26 14:10:14 -0500

    Created App component scaffolding

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation></Navigation>
+        {/* <Route path='/' exact component={Home}></Route>
+        <Route path='/mac' component={Mac}></Route>
+        <Route path='/ipad' component={Ipad}></Route>
+        <Route path='/iphone' component={Iphone}></Route>
+        <Route path='/watch' component={Watch}></Route>
+        <Route path='/tv' component={Tv}></Route>
+        <Route path='/music' component={Music}></Route>
+        <Route path='/support' component={Support}></Route>
+        <Footer></Footer> */}
       </div>
     );
   }
